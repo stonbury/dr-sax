b57332d213030075194221e570eba162992da2b1
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-03-03 15:27:49 +0000

    Use sax-js instead of htmlparser2

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
 var DrSax = require('./lib/drsax');
-var StreamingInterface = require('./lib/stream');
-
-// add in streaming
-DrSax.stream = function(options){
-  return new StreamingInterface(options);
-};
 
 module.exports = DrSax;
